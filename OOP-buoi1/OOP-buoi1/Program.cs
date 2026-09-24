6b56d42d7d15dea7dc71b1076661b1cecd71feee
LazycoderPHD <phatdao2309@gmail.com>
2026-09-24 10:19:03 +0700

    bai1 xong

--- OOP-buoi1/OOP-buoi1/Program.cs
+++ OOP-buoi1/OOP-buoi1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOP_buoi1;
+using System;
+
+namespace BaiTap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            SinhVien sv = new SinhVien();
+
+            Console.WriteLine("=== ENTER STUDENT INFORMATION ===");
+            sv.NhapThongTin();
+
+            Console.WriteLine("\n=== STUDENT INFORMATION DISPLAY ===");
+            sv.HienThiThongTin();
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
